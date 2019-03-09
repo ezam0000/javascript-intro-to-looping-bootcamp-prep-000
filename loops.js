d2f68818bc43dfb59065bb83ebd3888f520173c2
esteban zambrano <ezam0000@gmail.com>
2019-03-09 12:12:45 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+var i=0
+
+function forLoop(array){
+  [loopBody];
+  for (let i=0;i<25;i++)
+
+  if (i===1)
+  console.log("I am 1 strange loop.")
+  
+  else 
+  console.log('I am ${i} strange loops' )
+  return(array)
+}
